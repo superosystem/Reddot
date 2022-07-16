cf9cd45adf9361ddc2ff1dd0b2003b66e5c0e285
gusrylmubarok <gusrylmubarok@gmail.com>
2022-07-16 22:17:19 +0700

    Swagger

--- src/main/java/com/gusrylmubarok/reddit/backend/BackendApplication.java
+++ src/main/java/com/gusrylmubarok/reddit/backend/BackendApplication.java
@@ -1,13 +1,15 @@
 package com.gusrylmubarok.reddit.backend;
 
+import com.gusrylmubarok.reddit.backend.configuration.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.util.Map;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
