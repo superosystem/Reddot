04bd7028c2077684f64c31db065aa9bbd53f2eb9
gusrylmubarok <gusrylmubarok@gmail.com>
2022-07-19 20:47:55 +0700

    Refactor

--- src/main/java/com/gusrylmubarok/reddit/backend/exceptions/SubredditNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gusrylmubarok.reddit.backend.exceptions;
-
-public class SubredditNotFoundException extends RuntimeException {
-    public SubredditNotFoundException(String message) {
-        super(message);
-    }
-}
-
